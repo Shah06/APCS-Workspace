5a69d203f32a4498fdcbded71e3556de1e122065
atharva <atharva@nobody.com>
2019-03-22 11:15:09 -0700

    asdf

--- workspace/NetflixPrize/src/MovieLensCSVTranslator.java
+++ workspace/NetflixPrize/src/MovieLensCSVTranslator.java
@@ -3,6 +3,13 @@ public class MovieLensCSVTranslator {
 	
 	public Movie translateMovie(String line) {
 		// TODO turn into Movie object
+		// pull title
+		String movieTitle = line.split(",", 2)[1]; // cuts out title and comma
+		
+		
+		// pull genres
+		String[] movieGenres;
+		
 		
 		return null;
 	}
