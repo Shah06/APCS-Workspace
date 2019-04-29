fdb5e55dab4fc0dcfb8967d1a6ba475ac17c4c53
ashah766 <ashah766@hhs.win.fuhsd.org>
2019-04-29 12:14:58 -0700

    added 3d example

---
+++ workspace/3d test/src/DrawingSurface.java
@@ -0,0 +1,18 @@
+import processing.core.*;
+
+public class DrawingSurface extends PApplet {
+
+	public void settings() {
+	  size(640, 360, P3D);
+	}
+
+	public void draw() {
+	  background(0);
+	  camera(mouseX, height/2, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
+	  translate(width/2, height/2, -100);
+	  stroke(255);
+	  noFill();
+	  box(200);
+	}
+	
+}
---
+++ workspace/3d test/src/Main.java
@@ -0,0 +1,9 @@
+import processing.core.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		PApplet.main("DrawingSurface");
+	}
+	
+}
