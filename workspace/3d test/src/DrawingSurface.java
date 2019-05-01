0884193b9d8d8e729bfa89fd786d65699980c190
atharva <atharva@nobody.com>
2019-04-30 23:33:29 -0700

    added a camera library to the 3d test project

--- workspace/3d test/src/DrawingSurface.java
+++ workspace/3d test/src/DrawingSurface.java
@@ -1,18 +1,55 @@
 import processing.core.*;
+import queasycam.*;
 
 public class DrawingSurface extends PApplet {
 
+	public static int SCALING_FACTOR = 3;
+	
+	// image
+	PImage img;
+	QueasyCam cam;
+	
 	public void settings() {
-	  size(640, 360, P3D);
+	  size(400*SCALING_FACTOR, 400*SCALING_FACTOR, P3D);
+	}
+	
+	public void setup() {
+		img = loadImage("Rock.jpg");
+		textureMode(NORMAL);
+		background(100);
+		rectMode(CENTER);
+		fill(51);
+		stroke(255);
+		cam = new QueasyCam(this);
+		cam.speed = 5;
+		cam.sensitivity = 0.3f;
 	}
 
 	public void draw() {
-	  background(0);
-	  camera(mouseX, height/2, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
-	  translate(width/2, height/2, -100);
-	  stroke(255);
-	  noFill();
-	  box(200);
+		
+//		pushMatrix();
+//		translate(width/2, height/2, 0);
+//		rotateZ(PI/8);
+//		scale(SCALING_FACTOR); // scale should always directly precede shapes
+//		rect(0, 0, 100, 100);
+//		popMatrix();
+		
+		// set the camera to rotate with the mouse
+		
+		background(0);
+		pushMatrix();
+		scale(SCALING_FACTOR);
+		box(200);
+		popMatrix();
+		
+		
+//		pushMatrix();
+//		translate(width/2, height/2, 0);
+//		rotateY(PI/8);
+//		scale(SCALING_FACTOR);
+//		box(50, 50, 50);
+//		popMatrix();
+		
 	}
 	
 }
